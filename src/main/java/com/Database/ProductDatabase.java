b03ca59f6395cb69b30b7ca71d2c69dd5873d7ab
Jon Graf <jongraf1@gmail.com>
2019-03-13 22:41:07 -0500

    added method to query the database to get products by their respective types

--- src/main/java/com/Database/ProductDatabase.java
+++ src/main/java/com/Database/ProductDatabase.java
@@ -5,7 +5,12 @@ import com.models.Product;
 import java.util.List;
 
 public class ProductDatabase {
+    
     public List<Product> getProductsByLocation(String productLocation) {
+        return null;
+    }
+
+    public List<Product> getProductByType(String productType) {
         return null;
     }
 }
