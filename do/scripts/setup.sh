9e87d991e0c9de2357bdb63f684690480e69dc18
Jon Graf <jongraf1@gmail.com>
2019-05-02 15:29:35 -0500

    fixed bash commands to install postgres and call sql init file

--- do/scripts/setup.sh
+++ do/scripts/setup.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+POSTGRES_USER="postgres"
+POSTGRES_PASS="password"
+SQL_FILE="Init.sql"
+SQL_FILE_LOC="$DIR/$SQL_FILE"
 
 function setEnvVars {
     echo "----------Env Vars----------"
     export PATH="$PATH:C:\Program Files\PostgreSQL\10\bin"
+    export PGPASSWORD="$POSTGRES_PASS"
 }
 
 function createDatabase {
-    PSQL_CONNECTION_CMD="psql -U postgreSQL"
-    cmd //c $PSQL_CONNECTION_CMD
-
-    CREATE_DB_CMD="psql CREATE DATABASE VendingMachine WITH ENCODING 'UTF8'"
-    cmd //c $CREATE_DB_CMD
+    echo "----------Creating Database----------"
+    PSQL_CMD="psql -U $POSTGRES_PASS -f $SQL_FILE_LOC"
+    cmd //c $PSQL_CMD
 }
 
 setEnvVars
