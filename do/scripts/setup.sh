7fe321db6576f45703e9791ac377685b61b0acef
Jon Graf <jongraf1@gmail.com>
2019-05-06 17:34:10 -0500

    found error in setup file and renamed duplicated variable causing conflict

--- do/scripts/setup.sh
+++ do/scripts/setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 POSTGRES_USER="postgres"
 POSTGRES_PASS="password"
 SQL_FILE="Init.sql"
-SQL_FILE_LOC="$DIR/$SQL_FILE"
+SQL_FILE_LOC="$CURRENT_DIR/$SQL_FILE"
 
 function setEnvVars {
-    echo "----------Env Vars----------"
+    echo "----------Postgres Env Vars----------"
     export PATH="$PATH:C:\Program Files\PostgreSQL\10\bin"
     export PGPASSWORD="$POSTGRES_PASS"
 }
 
 function createDatabase {
     echo "----------Creating Database----------"
-    PSQL_CMD="psql -U $POSTGRES_PASS -f $SQL_FILE_LOC"
+    PSQL_CMD="psql -U $POSTGRES_USER -f $SQL_FILE_LOC"
     cmd //c $PSQL_CMD
 }
 
