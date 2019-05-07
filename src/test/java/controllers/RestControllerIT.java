54e718a35075914b15ae66d635aedb994fd0ca4f
Jon Graf <jongraf1@gmail.com>
2019-05-06 23:24:35 -0500

    WIP: working to full end to end test working for product purchase

--- src/test/java/controllers/RestControllerIT.java
+++ src/test/java/controllers/RestControllerIT.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import com.Application;
+import com.models.RequestProduct;
+import com.models.VendProduct;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +11,10 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+
+import static com.models.Coin.DOLLAR;
+import static com.models.Coin.QUARTER;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
@@ -32,6 +38,22 @@ public class RestControllerIT {
         HttpStatus status = response.getStatusCode();
 
         assertEquals(HttpStatus.OK, status);
+    }
+
+    @Test
+    public void purchaseProduct_ShouldSuccessfullyPurchaseProduct() {
+        RequestProduct requestProduct = new RequestProduct();
+        requestProduct.setProductLocation("B2");
+        requestProduct.setInsertedCoins(Arrays.asList(DOLLAR, QUARTER, QUARTER));
+
+        ResponseEntity<VendProduct> response = restTemplate.postForEntity(
+                createURLWithPort("/purchaseProduct"), requestProduct, VendProduct.class);
+
+//        VendProduct response = restTemplate.postForObject(
+//                createURLWithPort("/purchaseProduct"), requestProduct, VendProduct.class);
+
+//        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Pepsi", response.getBody().getProduct().getName());
     }
 
     private String createURLWithPort(String uri) {
