072afac88221944618c8ebc57b594f138dc87217
Jon Graf <jongraf1@gmail.com>
2019-05-06 22:28:33 -0500

    commented out flyway migration from code

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String [] args) {
+//        Flyway flyway = Flyway.configure().dataSource("jdbc:h2:file:./target/foobar", "sa", null).load();
+
+//        flyway.migrate();
         SpringApplication.run(Application.class, args);
     }
 }
