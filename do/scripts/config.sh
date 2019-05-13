7f9c769b268d8d22b12cd0ee3b6aea1e29ad172e
Jon Graf <jongraf1@gmail.com>
2019-05-12 20:45:47 -0500

    config script will now download and install docker

--- do/scripts/config.sh
+++ do/scripts/config.sh
@@ -1,42 +1,24 @@
 #!/bin/bash
 
-DATABASE_VERSION="1.1"
-
-PRESENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORK_DIR=`mktemp -d -p "$USERPROFILE"`
-PSQL_USER="postgres"
-PSQL_PASS="password"
-FLYWAY_VERSION="5.2.4"
-FLYWAY_TEMP_DIR="$WORK_DIR\flyway.zip"
-FLYWAY_DIR="$USERPROFILE\flyway"
-FLYWAY_LOC="$FLYWAY_DIR\flyway-$FLYWAY_VERSION"
+DOCKER_TEMP_DIR="$WORK_DIR\docker.exe"
 
-function downloadFlyway {
+function downloadDocker {
     if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
         echo "Could not create temp directory!"
         exit 1
     else
-        echo "----------Downloading Flyway----------"
-        curl -L -o $FLYWAY_TEMP_DIR "https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/$FLYWAY_VERSION/flyway-commandline-$FLYWAY_VERSION-windows-x64.zip"
+        echo "----------Downloading Docker----------"
+        curl -L -o $DOCKER_TEMP_DIR "https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe"
     fi
 }
 
-function extractFlyway {
-    echo "----------Extracting Flyway----------"
-    unzip $FLYWAY_TEMP_DIR -d $FLYWAY_DIR
-}
-
-function setEnvVars {
-    echo "----------Flyway Env Vars----------"
-    export PATH=$PATH:$FLYWAY_LOC
-}
-
-function migrateDatabase {
-    echo "----------Migrating Database----------"
-    MIGRATIONS_DIR="filesystem:..\..\db\migration"
-    pushd $PRESENT_DIR
-    flyway migrate -user=$PSQL_USER -password=$PSQL_PASS -url="jdbc:postgresql://localhost:5432/vending_machine" -target=$DATABASE_VERSION -locations=$MIGRATIONS_DIR
-    popd
+function installingDocker {
+    echo "----------Installing Docker----------"
+    TEST_LOC="$USERPROFILE/docker.exe"
+    INSTALL_CMD="$TEST_LOC"
+#    INSTALL_CMD="$DOCKER_TEMP_DIR"
+    cmd //c $INSTALL_CMD
 }
 
 function cleanupTempDir {
@@ -44,15 +26,12 @@ function cleanupTempDir {
     rm -rf $WORK_DIR
 }
 
-if [[ $PATH == *"flyway"* ]]; then
-    echo "----------Flyway Already Installed----------"
-    migrateDatabase
+if [[ $PATH == *"docker"* ]]; then
+    echo "----------Docker Already Installed----------"
 else
-    downloadFlyway
-    extractFlyway
-    setEnvVars
-    migrateDatabase
+#    downloadDocker
+    installingDocker
 fi
 
 
-trap cleanupTempDir EXIT
+#trap cleanupTempDir EXIT
