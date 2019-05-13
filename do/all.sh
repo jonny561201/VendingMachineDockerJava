5fe2c78ee390880752decf27da4ae6ad460a1385
Jon Graf <jongraf1@gmail.com>
2019-05-12 20:48:01 -0500

    cleaned up docker install scripts

--- do/all.sh
+++ do/all.sh
@@ -16,8 +16,6 @@ if isAdmin $1
     then
         echo "----------terminal is running as admin----------"
         source "$DIR/scripts/bootstrap.sh"
-        source "$DIR/scripts/setup.sh"
-        source "$DIR/scripts/config.sh"
     else
         echo "Terminal must be running as Administrator. Open new terminal."
 fi
--- do/scripts/bootstrap.sh
+++ do/scripts/bootstrap.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-BOOTSTRAP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-WORK_DIR=`mktemp -d -p "$BOOTSTRAP_DIR"`
-FILE_NAME="PostGres.exe"
-FILE_PATH="$WORK_DIR/$FILE_NAME"
+WORK_DIR=`mktemp -d -p "$USERPROFILE"`
+DOCKER_TEMP_DIR="$WORK_DIR\docker.exe"
 
-
-function cleanupTempDir {
-    rm -rf "$WORK_DIR"
-    echo "----------Deleted temp directory ----------"
+function downloadDocker {
+    if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
+        echo "Could not create temp directory!"
+        exit 1
+    else
+        echo "----------Downloading Docker----------"
+        curl -L -o $DOCKER_TEMP_DIR "https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe"
+    fi
 }
 
-function downloadInstaller {
-    echo "----------Downloading $FILE_NAME----------"
-    curl -L -o $FILE_PATH https://get.enterprisedb.com/postgresql/postgresql-10.7-2-windows-x64.exe
-    installPostGres
-}
-
-function installPostGres {
-    echo "----------Installing PostGres----------"
-    INSTALL_CMD="$FILE_PATH  --unattendedmodeui minimal --mode unattended --superpassword "password" --servicename "postgreSQL" --servicepassword "password" --serverport 5432"
+function installingDocker {
+    echo "----------Installing Docker----------"
+    TEST_LOC="$DOCKER_TEMP_DIR/docker.exe"
+    INSTALL_CMD="$DOCKER_TEMP_DIR"
     cmd //c $INSTALL_CMD
 }
 
-if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
-    echo "Could not create temp directory!"
-    exit 1
+function cleanupTempDir {
+    echo "----------Cleanup Temp Dir----------"
+    rm -rf $WORK_DIR
+}
+
+if [[ $PATH == *"docker"* ]]; then
+    echo "----------Docker Already Installed----------"
 else
-    echo "----------Created Directory: $WORK_DIR"
-    downloadInstaller
+    downloadDocker
+    installingDocker
 fi
 
 
-#register cleanupTempDir function to get called on EXIT
-trap cleanupTempDir EXIT
+#trap cleanupTempDir EXIT
