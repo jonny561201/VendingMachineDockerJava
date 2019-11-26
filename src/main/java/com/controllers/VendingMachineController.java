dd0f0a88873c5dd6e3d48e31259a262861a0f971
Jon Graf <jongraf1@gmail.com>
2019-11-26 10:35:12 -0600

    refactored out validation method in vending machine controller

--- src/main/java/com/controllers/VendingMachineController.java
+++ src/main/java/com/controllers/VendingMachineController.java
@@ -29,17 +29,20 @@ public class VendingMachineController {
                 .filter(CoinService::isValidCoin)
                 .collect(Collectors.toList());
         BigDecimal funds = coinService.countChange(validCoins);
-
-        if(!productService.isProductAvailable(productSelection)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product Unavailable");
-        }
         BigDecimal productCost = productService.getProductCost(productSelection);
-        if (!productService.hasSufficientFunds(productCost, funds)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Insufficient Funds");
-        }
+        validatePurchase(productSelection, funds, productCost);
         List<Coin> change = coinService.returnChange(productCost, funds);
 
         return createResponse(change);
+    }
+
+    private void validatePurchase(String productSelection, BigDecimal funds, BigDecimal productCost) {
+        if (!productService.isProductAvailable(productSelection)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product Unavailable");
+        }
+        if (!productService.hasSufficientFunds(productCost, funds)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Insufficient Funds");
+        }
     }
 
     private VendProduct createResponse(List<Coin> change) {
