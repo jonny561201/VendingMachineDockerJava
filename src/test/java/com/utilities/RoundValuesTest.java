da99ed2804be498544f8483f34a0e80b1cbc7ef8
Jon Graf <jongraf1@gmail.com>
2019-11-27 11:55:39 -0600

    backfilled test coverage to ensure rounding up and down for big decimal

--- src/test/java/com/utilities/RoundValuesTest.java
+++ src/test/java/com/utilities/RoundValuesTest.java
@@ -12,8 +12,26 @@ public class RoundValuesTest {
     public void round_ShouldRoundValueToTwoDecimals() {
         var valueToRound = new BigDecimal("4.11000008");
         var actual = RoundValues.round(valueToRound);
-        
+
         var expectedValue = new BigDecimal("4.11");
+        assertEquals(expectedValue, actual);
+    }
+
+    @Test
+    public void round_ShouldRoundValueBelowFiveDown() {
+        var valueToRound = new BigDecimal("3.4444444");
+        var actual = RoundValues.round(valueToRound);
+
+        var expectedValue = new BigDecimal("3.44");
+        assertEquals(expectedValue, actual);
+    }
+
+    @Test
+    public void round_ShouldRoundValueAboveFiveUp() {
+        var valueToRound = new BigDecimal("3.446666");
+        var actual = RoundValues.round(valueToRound);
+
+        var expectedValue = new BigDecimal("3.45");
         assertEquals(expectedValue, actual);
     }
 
