86bac68c12bdde07e9467578319781648bddc2fa
Jon Graf <jongraf1@gmail.com>
2020-07-28 13:42:09 -0500

    utilizing constructor based injection for integration testing

--- src/test/java/repository/ProductDatabaseTest.java
+++ src/test/java/repository/ProductDatabaseTest.java
@@ -5,8 +5,10 @@ import com.repository.ProductDatabase;
 import com.models.Product;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -20,7 +22,8 @@ public class ProductDatabaseTest {
     @Test
     public void getProductsByLocation_ShouldReturnProductsFromTable() {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
-        ProductDatabase database = context.getBean(ProductDatabase.class);
+        JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+        ProductDatabase database = new ProductDatabase(jdbcTemplate);
 
         String productLocation = "C3";
         List<Product> actualProducts = database.getProductsByLocation(productLocation);
